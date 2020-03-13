ae12b5af4caaa351a994efa893f905efc72d8a5b
Vadym <vadym.tyshchenko5@gmail.com>
2020-03-13 13:35:28 +0200

    Parsing and generation
    Models of input YAML,
    models of output YAML,
    parsing input YAML,
    generating output YAML,
    fixing comments

--- main.go
+++ main.go
@@ -2,10 +2,52 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
 )
+
+// Models of input YAML
+
+type Info struct {
+	Title       string `yaml:"title"`
+	Version     string `yaml:"version"`
+	Description string `yaml:"description"`
+}
+
+type Server struct {
+	URL string "json:`url`"
+}
+
+type Method struct {
+	Description string                 `yaml:"description"`
+	OperationID string                 `yaml:"operationId"`
+	Responses   map[string]interface{} `yaml:"responses"`
+	RBACFeature []string               `yaml:"x-rbac-feature"`
+}
+
+type OpenAPISpec struct {
+	Version string                       `yaml:"openapi"`
+	Info    Info                         `yaml:"info"`
+	Servers []Server                     `yaml:"servers"`
+	Paths   map[string]map[string]Method `yaml:"paths"`
+}
+
+// Models of output YAML
+
+type Feature struct {
+	FeatureName string              `yaml:"feature"`
+	ID          string              `yaml:"id"`
+	Description string              `yaml:"description"`
+	Endpoints   []map[string]string `yaml:"endpoints"`
+}
+
+type ServiceTmpl struct {
+	ServiceName string `yaml:"serviceName"`
+	Features    []Feature
+}
 
 func main() {
 	// Define flags
@@ -15,27 +57,47 @@ func main() {
 	// Parse command line flags
 	flag.Parse()
 
-	fmt.Printf("Input path: %s\nOutput path: %s\n", *inputFilePath, *outputFilePath)
-
-	// Reading from file
+	// Read from input file
 	inputSpec, err := ioutil.ReadFile(*inputFilePath)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	fmt.Printf("Input Open API spec:\n%s", string(inputSpec))
+	// Parse input file
+	var openAPISpec OpenAPISpec
+	var serviceTmpl ServiceTmpl
 
-	// Creating new file
+	yaml.Unmarshal(inputSpec, &openAPISpec)
+
+	// Generate output YAML
+	serviceTmpl.ServiceName = openAPISpec.Info.Title // Service name
+
+	for pathKey, path := range openAPISpec.Paths {
+		for methodKey, method := range path {
+			for _, feature := range method.RBACFeature {
+				var endpoints []map[string]string
+				endpoint := make(map[string]string)
+				endpoint[pathKey] = methodKey
+				endpoints = append(endpoints, endpoint)
+
+				serviceTmpl.Features = append(serviceTmpl.Features, Feature{
+					FeatureName: strings.ReplaceAll(feature, "_", " "),
+					ID:          feature,
+					Description: method.Description,
+					Endpoints:   endpoints,
+				})
+			}
+		}
+	}
+
+	// Creating output file
 	outputFile, err := os.Create(*outputFilePath)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// Writing into new file
-	_, err = outputFile.WriteString(string(inputSpec))
-	if err != nil {
-		panic(err.Error())
-	}
+	// Encoding and writing YAML into new file
+	yaml.NewEncoder(outputFile).Encode(serviceTmpl)
 
 	// Closing new file
 	err = outputFile.Close()
